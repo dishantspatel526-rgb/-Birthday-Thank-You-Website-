aca9bf56bc5e5d7dddadd633b8a92038389546da
TumharaGitHubUsername <tumharaemail@gmail.com>
2026-07-16 17:14:09 +0530

    Fix countdown

--- scipt.js
+++ scipt.js
@@ -3,47 +3,81 @@ const content = document.getElementById("content");
 
 let number = 3;
 
-
-let timer = setInterval(()=>{
-
-    number--;
+let timer = setInterval(() => {
 
     countdown.innerHTML = `
         <h1>${number}</h1>
     `;
 
+    number--;
 
-    if(number === 0){
+    if(number < 0){
 
         clearInterval(timer);
 
-        countdown.style.display="none";
+        countdown.style.display = "none";
 
-        content.style.display="block";
-
-        startAnimation();
+        content.style.display = "block";
 
     }
-
 
 },1000);
 
 
-document.getElementById("surpriseBtn")
-.addEventListener("click",()=>{
+// Gift Box
 
-    // Confetti Blast
-    confetti({
+const gift = document.getElementById("giftBox");
+const cake = document.getElementById("cakeBox");
+const finalMsg = document.getElementById("finalMessage");
 
-        particleCount:200,
 
-        spread:120,
+if(gift){
 
-        origin:{
-            y:0.6
-        }
+gift.addEventListener("click",()=>{
 
-    });
+    gift.style.display="none";
 
+    cake.style.display="block";
+
+    finalMsg.style.display="block";
+
+
+    if(typeof confetti !== "undefined"){
+
+        confetti({
+            particleCount:200,
+            spread:150
+        });
+
+    }
 
 });
+
+}
+
+
+// Surprise Button
+
+const btn = document.getElementById("surpriseBtn");
+
+
+if(btn){
+
+btn.addEventListener("click",()=>{
+
+
+    if(typeof confetti !== "undefined"){
+
+        confetti({
+            particleCount:200,
+            spread:120,
+            origin:{
+                y:0.6
+            }
+        });
+
+    }
+
+});
+
+}
